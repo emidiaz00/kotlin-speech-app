6ec6c2d3c9e076f6cfe2839281c57992b33f4060
emidiaz00 <emildsn00@gmail.com>
2022-09-27 16:38:06 -0300

    agrego edit text, modifico estilos

--- app/src/main/java/com/example/hellokotlin/MainActivity.kt
+++ app/src/main/java/com/example/hellokotlin/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.hellokotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.speech.tts.TextToSpeech
-import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import java.util.*
@@ -24,16 +23,16 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener{
 
     }
     private fun speak() {
-        var message: String = findViewById<TextView>(R.id.txt_hello).text.toString()
+        var message: String = findViewById<TextView>(R.id.tvStatus).text.toString()
         tts!!.speak(message, TextToSpeech.QUEUE_FLUSH, null, "")
     }
 
     override fun onInit(status: Int) {
         if (status == TextToSpeech.SUCCESS) {
-            findViewById<TextView>(R.id.txt_hello).text = "Ready!"
+            findViewById<TextView>(R.id.tvStatus).text = "Ready!"
             tts!!.setLanguage(Locale.US)
         } else {
-            findViewById<TextView>(R.id.txt_hello).text = "No disponible"
+            findViewById<TextView>(R.id.tvStatus).text = "No disponible"
         }
     }
 }
