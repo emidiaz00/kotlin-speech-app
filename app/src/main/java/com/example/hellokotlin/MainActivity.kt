9c8056b861f3d9481b33e8ed3dfedf915dc1ed02
emidiaz00 <emildsn00@gmail.com>
2022-09-27 13:36:09 -0300

    implemento interfaz text to speech

--- app/src/main/java/com/example/hellokotlin/MainActivity.kt
+++ app/src/main/java/com/example/hellokotlin/MainActivity.kt
@@ -2,19 +2,29 @@ package com.example.hellokotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
 import android.util.Log
 import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener{
+
+    var tts: TextToSpeech? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        tts = TextToSpeech(this, this)
+
         var txt: String = findViewById<TextView>(R.id.txt_hello).text.toString()
         Log.i("mensaje: ", txt)
+    }
 
-
-
-
+    override fun onInit(status: Int) {
+        if (status == TextToSpeech.SUCCESS) {
+            findViewById<TextView>(R.id.txt_hello).text = "Listo!"
+        } else {
+            findViewById<TextView>(R.id.txt_hello).text = "No disponible"
+        }
     }
 }
